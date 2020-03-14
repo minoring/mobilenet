5df532a59ad6158b3fbca0d728d382372b570206
minoring <minho1350@gmail.com>
2020-03-14 14:28:31 -0700

    Implement inception preprocessing

--- slim/preprocessing/inception_preprocessing.py
+++ slim/preprocessing/inception_preprocessing.py
@@ -248,6 +248,70 @@ def preprocess_for_train(image,
           lambda x, ordering: distort_color(x, ordering, fast_mode),
           num_cases=num_distort_cases)
 
+      if use_grayscale:
+        distorted_image = tf.image.rgb_to_grayscale(distorted_image)
+
+      if add_image_summaries:
+        tf.summary.image('final_distorted_image',
+                         tf.expand_dims(distorted_image, 0))
+
+      distorted_image = tf.subtract(distorted_image, 0.5)
+      distorted_image = tf.multiply(distorted_image, 2.0)
+      return distorted_image
+
+
+def preprocess_for_eval(image,
+                        height,
+                        width,
+                        central_fraction=0.875,
+                        scope=None,
+                        central_crop=True,
+                        use_grayscale=False):
+  """Prepare one image for evaluation.
+
+  If height and width are specified it would output an image with taht size by
+  applying resize_bilinear.
+
+  If central_fraction is specified it would crop the central fraction of the
+  input image.
+
+  Args:
+  image: 3-D Tensor of image. If dtype is tf.float32 then the range should be
+    [0, 1], otherwise it would converted to tf.float32 assuming that the range
+    is [0, MAX], where MAX is largest positive representable number for
+    int(8/16/32) data type (see `tf.image.convert_image_dtype` for details).
+  height: integer
+  width: integer
+  central_fraction: Optional Float, fraction of the image to crop.
+  scope: Optional scope for name_scope.
+  central_crop: Enable central cropping of images during preprocessing for
+    evaluation.
+  use_grayscale: Whether to convert the image from RGB to grayscale.
+
+  Returns:
+    3-D float Tensor of prepared image.
+  """
+  with tf.name_scope(scope, 'eval_image', [image, height, width]):
+    if image.dtype != tf.float32:
+      image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    if use_grayscale:
+      image = tf.image.rgb_to_grayscale(image)
+
+    # Crop the central region of the image with an area containing 87.5% of
+    # the original image.
+    if central_crop and central_fraction:
+      image = tf.image.central_crop(image, central_fration=central_fraction)
+
+    if height and width:
+      # Resize the image to the specified height and width.
+      image = tf.expand_dims(image, 0)
+      image = tf.image.resize_bilinear(image, [height, width],
+                                       align_corners=False)
+      image = tf.squeeze(image, [0])
+
+    image = tf.subtract(image, 0.5)
+    image = tf.multiply(image, 2.0)
+    return image
 
 
 def preprocess_image(image,
